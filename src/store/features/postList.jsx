3abbffdd1e6e76bc9a35fb04aa6503114aebd7f1
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-07-28 09:54:13 +0530

    Code Cleanup

--- src/store/features/postList.jsx
+++ src/store/features/postList.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postSlice";
-import timeAgo from "./timeAgo";
 import TimeAgo from "./timeAgo";
 
 export default function PostList() {
